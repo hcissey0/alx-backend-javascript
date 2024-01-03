414f820d538098cef169446569e3e3bf82337a64
Hassan Cissey <hcissey0@gmail.com>
2024-01-03 06:36:41 +0000

    The wonders of spread syntax

---
+++ 0x00-ES6_basic/11-main.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+// eslint-disable-next-line quotes
+console.log(createEmployeesObject("Software", ["Bob", "Sylvie"]));
