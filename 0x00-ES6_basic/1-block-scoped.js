bb2c86e9bbb35c4e875322f3170c679f26317e3b
Hassan Cissey <hcissey0@gmail.com>
2024-01-03 06:03:04 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  task = false;
+  task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    var task = false;
+    var task2 = true;
   }
 
   return [task, task2];
