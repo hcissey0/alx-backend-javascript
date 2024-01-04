e5874ebcc9b4b82bfc934097eca86580585d5540
Hassan Cissey <hcissey0@gmail.com>
2024-01-04 05:32:12 +0000

    Promise 2

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve({status: 200, body: 'success'})
-  })
+  return new Promise((resolve, reject) => {})
 }
