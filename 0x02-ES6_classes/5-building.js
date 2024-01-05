f6efaa96deea6093a232df3083d187dcd3deaab2
Hassan Cissey <hcissey0@gmail.com>
2024-01-05 04:25:10 +0000

    Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,14 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
