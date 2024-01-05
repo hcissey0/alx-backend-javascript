2ab5738bf18cca7380f9def1e31851cab28bfabd
Hassan Cissey <hcissey0@gmail.com>
2024-01-05 04:52:38 +0000

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-use-before-define */
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -16,12 +12,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -38,11 +28,20 @@ export class StudentHolberton {
     return this._holbertonClass;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   get fullStudentDescription() {
-    const self = this;
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
