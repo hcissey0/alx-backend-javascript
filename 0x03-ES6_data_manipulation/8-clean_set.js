e2b78dfd09213c8567d074533cf9398a3036388d
Hassan Cissey <hcissey0@gmail.com>
2024-01-09 11:03:56 +0000

    clean

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,6 @@
 export default function cleanSet(set, startString) {
-  return startString
-    ? [...set]
-      .filter((val) => val.startsWith(startString))
-      .map((val) => val.slice(startString.length))
-      .join('-')
-    : '';
+  return [...set]
+		.filter((val) => val.startsWith(startString))
+		.map((val) => val.slice(startString.length))
+		.join('-');
 }
