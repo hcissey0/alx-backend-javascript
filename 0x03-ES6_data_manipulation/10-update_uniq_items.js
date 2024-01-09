cdcbeb98f76af97858af50ca5e7b77c3c7e1f951
Hassan Cissey <hcissey0@gmail.com>
2024-01-09 10:01:04 +0000

    update map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,12 @@
 export default function updateUniqueItems(map) {
-  if (!Map.isMap(map)) {
-    return new Error('Cannot process');
-  }
-  for (const [k, v] of map) {
-    if (v === 1) {
-      map.set(k, 100);
+  try {
+    for (const [k, v] of map) {
+      if (v === 1) {
+        map.set(k, 100);
+      }
     }
+    return map;
+  } catch (e) {
+    throw new Error('Cannot process');
   }
-  return map;
 }
