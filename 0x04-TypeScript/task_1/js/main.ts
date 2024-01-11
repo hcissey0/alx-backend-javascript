76304093169bd4ddf9a664fd8dc0d243c5bc8d92
Hassan Cissey <hcissey0@gmail.com>
2024-01-11 03:07:18 +0000

    task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,15 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacherFunction: PrintTeacherFunction = printTeacher;
+
+console.log(printTeacherFunction('John', 'Doe'));
