74ff11afafa7b7c5f07732299b2688528f2b38dd
Hassan Cissey <hcissey0@gmail.com>
2024-02-28 04:11:46 +0000

    reading file synchronously

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}\n`);
     process.exit();
   });
 } else {
