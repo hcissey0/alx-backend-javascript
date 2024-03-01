b66659787b0590f3a988f50db079e5f5ab8f5e14
Hassan Cissey <hcissey0@gmail.com>
2024-03-01 04:39:52 +0000

    round

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,25 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
+const { should } = require('chai');
 
 describe('calculateNumber', () => {
+  it('should round a', () => {
+    assert.equal(calculateNumber(2.2, 1), 3);
+    assert.equal(calculateNumber(2.5, 1), 4);
+    assert.equal(calculateNumber(2.7, 1), 4);
+  });
+
+  it('should round b', () => {
+    assert.equal(calculateNumber(1, 2.2), 3);
+    assert.equal(calculateNumber(1, 2.5), 4);
+    assert.equal(calculateNumber(1, 2.7), 4);
+  });
+
+  it('should round a and b', () => {
+    assert.equal(calculateNumber(2.2, 2.2), 4);
+    assert.equal(calculateNumber(2.5, 2.5), 6);
+    assert.equal(calculateNumber(2.7, 2.7), 6);
+  });
   it('should round numbers and return their sum', () => {
     assert.equal(calculateNumber(1, 3), 4);
     assert.equal(calculateNumber(1, 3.7), 5);
@@ -9,3 +27,4 @@ describe('calculateNumber', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
+
---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,8 @@
+function calculateNumber(a, b, type) {
+    const ra = Math.round(a);
+    const rb = Math.round(b);
+    
+    return ra + rb;
+}
+
+module.exports = calculateNumber;
