8f5fa3f5671a17786b15cec6b7169f5a6749a090
Hassan Cissey <hcissey0@gmail.com>
2024-03-05 20:25:26 +0000

    sinon stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,18 +6,29 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 
 describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+  let calculateNumberStub;
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.returns(10);
+
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+    calculateNumberStub.restore();
+  });
+
   it('should call Utils.calculateNumber with correct arguments', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    const calculateNumberSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
     expect(calculateNumberStub.calledOnce).to.equal(true);
     expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.equal(true);
-    expect(calculateNumberSpy.calledOnce).to.equal(true);
-    expect(calculateNumberSpy.calledWith('The total is: 10')).to.equal(true);
 
-    calculateNumberSpy.restore();
-    calculateNumberStub.restore();
+    expect(consoleLogSpy.calledOnce).to.equal(true);
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.equal(true);
   });
+
 });
