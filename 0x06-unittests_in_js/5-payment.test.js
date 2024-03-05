3b6e16590cc6d290a32fd7b6454bec4a6bd69cb5
Hassan Cissey <hcissey0@gmail.com>
2024-03-05 20:32:05 +0000

    afterEach and beforeEach

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,5 +1,31 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment')
 
 
 describe('sendPaymentRequestToAPI', () => {
-  it('should test with 100 and 20')
-})
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should test with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 120');
+
+    sinon.assert.calledOnce(consoleLogSpy);
+  });
+
+  it('should log "The total is: 20" for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    sinon.assert.calledWith(consoleLogSpy, 'The total is: 20');
+
+    sinon.assert.calledOnce(consoleLogSpy);
+  });
+});
