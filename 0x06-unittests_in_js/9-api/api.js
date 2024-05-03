09d2c57b558aff0a554167379bf5fc62b08b865d
Hassan Cissey <hcissey0@gmail.com>
2024-05-03 04:24:26 +0000

    9-api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,6 +5,10 @@ const port = 7865;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system')
+})
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const id = req.params.id;
+    res.send(`Payment methods for cart ${id}`);
 }).listen(port, () => {
     console.log(`API available on localhost port 7865`);
 })
